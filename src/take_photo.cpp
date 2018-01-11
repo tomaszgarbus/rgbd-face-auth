041fed977b3f2ca4e08a34bcc83153096f7d18c9
Łukasz Raszkiewicz <lraszkiewicz@gmail.com>
2018-01-11 23:09:56 +0100

    Change photo filenames

--- src/take_photo.cpp
+++ src/take_photo.cpp
@@ -1,3 +1,6 @@
+#include <ctime>
+#include <chrono>
+#include <thread>
 #include <fstream>
 #include <iostream>
 
@@ -7,7 +10,27 @@
 
 #include "libkinect/libkinect.hpp"
 
+std::string PHOTOS_DIRECTORY = "../photos/";
+
 bool depthPhotoTaken, irPhotoTaken, rgbPhotoTaken;
+
+std::string getCurrentTime() {
+  auto currentTime = std::chrono::system_clock::now();
+  auto currentTimeAsTimeT = std::chrono::system_clock::to_time_t(currentTime);
+
+  char currentTimeChar[100];
+  std::strftime(currentTimeChar, sizeof(currentTimeChar),
+                "%Y-%m-%d-%H-%M-%S", std::gmtime(&currentTimeAsTimeT));
+  std::string currentTimeStr = currentTimeChar;
+
+  currentTimeStr += '-';
+  std::string milliseconds = std::to_string(
+      std::chrono::duration_cast<std::chrono::milliseconds>(
+          currentTime.time_since_epoch()).count() % 1000);
+  currentTimeStr += std::string(3 - milliseconds.length(), '0') + milliseconds;
+
+  return currentTimeStr;
+}
 
 class MyKinectDevice : public KinectDevice {
  public:
@@ -16,11 +39,14 @@ class MyKinectDevice : public KinectDevice {
     if (depthPhotoTaken && rgbPhotoTaken && irPhotoTaken) {
       exit(0);
     }
+    std::string filename = PHOTOS_DIRECTORY + getCurrentTime()
+        + "-kinect" + std::to_string(getKinectVersion()) + "-";
+
     if (frame.type == FrameType::DEPTH) {
       if (depthPhotoTaken) {
         return;
       }
-      std::ofstream depthFile("../photo_kinect1_depth.txt");
+      std::ofstream depthFile(filename + "depth.txt");
       for (int i = 0; i < frame.height; ++i) {
         for (int j = 0; j < frame.width; ++j) {
           depthFile << frame.data.depthOrIrData[frame.width * i + j] << '\t';
@@ -34,7 +60,7 @@ class MyKinectDevice : public KinectDevice {
       if (irPhotoTaken) {
         return;
       }
-      std::ofstream irFile("../photo_kinect1_ir.txt");
+      std::ofstream irFile(filename + "ir.txt");
       for (int i = 0; i < frame.height; ++i) {
         for (int j = 0; j < frame.width; ++j) {
           irFile << frame.data.depthOrIrData[frame.width * i + j] << '\t';
@@ -52,7 +78,7 @@ class MyKinectDevice : public KinectDevice {
           cv::Size(int(frame.width), int(frame.height)),
           CV_8UC3, frame.data.rgbData);
       cv::cvtColor(image, image, cv::COLOR_RGB2BGR);
-      cv::imwrite("../photo_kinect1_rgb.png", image);
+      cv::imwrite(filename + "rgb.png", image);
       rgbPhotoTaken = true;
       std::cout << "RGB photo taken.\n";
     }
