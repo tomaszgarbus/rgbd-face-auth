c4ab292f5c6671a641a9d3b62fc1b679a29b67c2
Tomasz Garbus <tgarbus@chromium.org>
2018-03-16 23:48:33 +0100

    PyCharm was complaining about PEP 8

--- exp/common/tools.py
+++ exp/common/tools.py
@@ -6,23 +6,24 @@ import matplotlib.pyplot as plt
 from skimage.filters.rank import entropy
 from skimage.morphology import disk
 
-image_size=50
+image_size = 64
 TYPES = 4
 
+
 def rgb_skin_check(R, G, B):
-    '''
+    """
         Based on "Human Skin Detection by Visible and Near-Infrared Imaging"
         by Yusuke Kanzawa, Yoshikatsu Kimura, Takashi Naito
 
         MVA2011 IAPR Conference on Machine Vision Applications, June 13-15, 2011, Nara, JAPAN
-    '''
-    '''
+    """
+    """
         [ Y]   [ 16]   [ 0.257,  0.504,  0.098]   [R]
         [Cb] = [128] + [-0.148, -0.291,  0.439] x [G]
         [Cr]   [128]   [ 0.439, -0.368, -0.071]   [B]
 
         skin_pixel implies: Cb in [77; 127] and Cr in [133; 173]
-    '''
+    """
 
     _  =  16 +    0.257 * R +    0.504 * G +    0.098 * B
     Cb = 128 + (-0.148) * R + (-0.291) * G +    0.439 * B
@@ -33,6 +34,7 @@ def rgb_skin_check(R, G, B):
 
 def load_color_image_from_file(filename):
     return np.array(PIL.Image.open(filename))
+
 
 def load_depth_photo(path):
     with open(path, 'rb') as f:
@@ -45,18 +47,22 @@ def load_depth_photo(path):
         photo = photo.reshape(height, width)
         return photo
 
+
 def change_image_mode(source_mode, output_mode, img):
     return np.array(PIL.Image.fromarray(img, mode=source_mode).convert(output_mode))
+
 
 def rgb_image_resize(img, size):
     tmp = PIL.Image.fromarray(img, mode='RGB')
     tmp = tmp.resize(size)
     return np.array(tmp)
 
+
 def gray_image_resize(img, size):
     tmp = PIL.Image.fromarray(img, mode='F')
     tmp = tmp.resize(size)
     return np.array(tmp)
+
 
 def color_image_to_face(img):
     coords = face_recognition.face_locations(img)
@@ -64,16 +70,19 @@ def color_image_to_face(img):
         (x1,y1,x2,y2) = coords[0]
         return rgb_image_resize(img[x1:x2,y2:y1], img.shape[:2])
     else:
-        return img# TODO: handle it
+        return img  # TODO: handle it
+
 
 def show_image(img):
     plt.imshow(img)
     plt.show()
 
+
 def show_gray_image(img):
     plt.gray()
     plt.imshow(img)
     plt.show()
+
 
 def rgb_image_to_gray_entropy(img):
     img = change_image_mode('RGBA', 'L', img)
