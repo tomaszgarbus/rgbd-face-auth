a7d13e31a3c861b57c2ed267d7840cb54b3beea8
Dominik Klemba <D.Klemba@students.mimuw.edu.pl>
2018-03-20 16:03:30 +0100

    New model, simple refatorization of code
    This code still (or maybe even more?) need refactorization.

--- exp/no_normalization/main.py
+++ exp/no_normalization/main.py
@@ -12,54 +12,110 @@ from common.db_helper import DB_LOCATION
 
 from common.tools import IMG_SIZE
 
-if __name__ == '__main__':
-    sys.setrecursionlimit(1000000)
+def get_model(model_id):
+    # TODO sensible creating models
+    if model_id == 0:
+        model = Sequential()
+        model.add(Conv2D(20,
+                     kernel_size=(6, 6),
+                     activation='relu',
+                     input_shape=(4 * IMG_SIZE, IMG_SIZE, 1)))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Conv2D(20,
+                     kernel_size=(6, 6),
+                     activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Conv2D(20,
+                     kernel_size=(6, 6),
+                     activation='relu'))
+        model.add(Flatten())
+        model.add(Dropout(0.7))
+        model.add(Dense(128, activation='relu'))
+        model.add(Dropout(0.7))
+        model.add(Dense(TOTAL_SUBJECTS_COUNT, activation='softmax'))
 
-    print("Loading data..")
+        return model
+
+    if model_id == 1:
+        model = Sequential()
+
+        #
+        model.add(Conv2D(16, 8*2,
+                     padding='same',
+                     activation='relu',
+                     input_shape=(4 * IMG_SIZE, IMG_SIZE, 1)))
+        model.add(Conv2D(16, 8*2,
+                     padding='same',
+                     activation='relu'))
+        model.add(MaxPooling2D(2))
+        #model.add(Dropout(0.825))
+
+        #
+        model.add(Conv2D(32, 8,
+                     padding='same',
+                     activation='relu'))
+        model.add(Conv2D(32, 8,
+                     padding='same',
+                     activation='relu'))
+        model.add(MaxPooling2D(2, 2))
+        #model.add(Dropout(0.825))
+
+        #
+        model.add(Flatten())
+
+        model.add(Dense(256, activation='relu'))
+        model.add(Dropout(0.2))
+        model.add(Dense(TOTAL_SUBJECTS_COUNT, activation='softmax'))
+
+        return model
+
+    raise ValueError("Incorrect model id")
+
+def get_no_normalization_models():
+    #TODO: move it to tools in smart way (argument with prefix name?)
     X_train = np.load(DB_LOCATION + '/gen/no_normalization_X_train.npy')
     Y_train = np.load(DB_LOCATION + '/gen/no_normalization_Y_train.npy')
     X_test = np.load(DB_LOCATION + '/gen/no_normalization_X_test.npy')
     Y_test = np.load(DB_LOCATION + '/gen/no_normalization_Y_test.npy')
+
+    return X_train, Y_train, X_test, Y_test
+
+if __name__ == '__main__':
+    sys.setrecursionlimit(1000000)
+
+    print("Loading data..")
+    X_train, Y_train, X_test, Y_test = get_no_normalization_models()
+
     TOTAL_SUBJECTS_COUNT = Y_test[0].shape[0]
     print("Loaded data")
     # If you want, display the first input image. It is already scaled to [0;1]
     # tools.show_image(X_train[0].reshape((TYPES * IMG_SIZE, IMG_SIZE)));
 
-    model = Sequential()
-    model.add(Conv2D(20,
-                     kernel_size=(6, 6),
-                     activation='relu',
-                     input_shape=(4 * IMG_SIZE, IMG_SIZE, 1)))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(20,
-                     kernel_size=(6, 6),
-                     activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(20,
-                     kernel_size=(6, 6),
-                     activation='relu'))
-    model.add(Flatten())
-    model.add(Dropout(0.7))
-    model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.7))
-    model.add(Dense(TOTAL_SUBJECTS_COUNT, activation='softmax'))
+    model_id = 0
+    print(sys.argv)
+    if len(sys.argv) > 1:
+        model_id = int(sys.argv[1])
+    model = get_model(model_id)
 
     model.compile(optimizer='adam',
                   loss='categorical_crossentropy',
-                  metrics=['categorical_accuracy'])
+                  metrics=['accuracy'])
 
     train_generator = ImageDataGenerator().flow(X_train, Y_train)
-    test_generator = ImageDataGenerator().flow(X_test, Y_test)
+    test_generator = ImageDataGenerator().flow(X_test, Y_test) #TODO(Tomek): <--- ????
 
     total_epochs = 0
+    epochs_per_round = 20
+
     while True:
         # 50 epochs is obviously not enough, repeat until convergence.
+        # TODO: Infinity while isn't best option...
         model.fit_generator(train_generator,
                             initial_epoch=total_epochs,
-                            epochs=total_epochs+50,
-                            steps_per_epoch=10,
+                            epochs=total_epochs+epochs_per_round,
                             verbose=True,
-                            validation_steps=1)
+                            validation_data=(X_test, Y_test)
+        )
         loss = model.evaluate(X_test, Y_test)
-        total_epochs += 50
+        total_epochs += epochs_per_round
         print("Epochs: %d;  Loss (on test set): %f;  Cat. accuracy: %f" % (total_epochs, loss[0], loss[1]))
