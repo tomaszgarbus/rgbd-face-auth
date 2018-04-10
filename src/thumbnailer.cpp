30a65e375a387e6e69c88125d2126096b3fb9074
Łukasz Raszkiewicz <lraszkiewicz@gmail.com>
2018-04-10 22:28:09 +0200

    Add IR photo support to thumbnailer

--- src/thumbnailer.cpp
+++ src/thumbnailer.cpp
@@ -6,6 +6,8 @@
 
 const float min_depth = 500.0;
 const float max_depth = 1500.0;
+const float max_ir_v1 = 1024.0;
+const float max_ir_v2 = 65535.0;
 
 int main(int argc, char **argv) {
    // argv[1] - input file
@@ -22,16 +24,36 @@ int main(int argc, char **argv) {
    }
    frame.resize(thumb_width, thumb_height);
    auto int_pixels = new uint8_t[thumb_width * thumb_height];
+   float max_ir = 0.0;
+   if (!frame.is_depth) {
+      for (size_t i = 0; i < thumb_width * thumb_height; ++i) {
+         max_ir = std::max(max_ir, frame.pixels->data()[i]);
+      }
+      if (max_ir <= 1024.0) { // Kinect v1
+         max_ir = max_ir_v1;
+      } else { // Kinect v2
+         max_ir = std::max(max_ir, max_ir_v2);
+      }
+   }
    for (size_t i = 0; i < thumb_width * thumb_height; ++i) {
-      float val = 255.0f * (frame.pixels->data()[i] - min_depth) / (max_depth - min_depth);
+      float val;
+      if (frame.is_depth) {
+         val = 255.0f * (frame.pixels->data()[i] - min_depth) / (max_depth - min_depth);
+      } else {
+         val = 255.0f * frame.pixels->data()[i] / max_ir;
+      }
       val = std::min(val, 255.0f);
       val = std::max(val, 0.0f);
       int_pixels[i] = static_cast<uint8_t>(val);
    }
    cv::Mat current_image(cv::Size(static_cast<int>(thumb_width), static_cast<int>(thumb_height)), CV_8UC1, int_pixels);
-   cv::Mat destination_image(cv::Size(static_cast<int>(thumb_width), static_cast<int>(thumb_height)), CV_8UC3);
-   cv::applyColorMap(current_image, destination_image, cv::COLORMAP_RAINBOW);
-   cv::imwrite(std::string(argv[2]) + ".png", destination_image);
+   if (frame.is_depth) {
+      cv::Mat destination_image(cv::Size(static_cast<int>(thumb_width), static_cast<int>(thumb_height)), CV_8UC3);
+      cv::applyColorMap(current_image, destination_image, cv::COLORMAP_RAINBOW);
+      cv::imwrite(std::string(argv[2]) + ".png", destination_image);
+   } else {
+      cv::imwrite(std::string(argv[2]) + ".png", current_image);
+   }
    rename((std::string(argv[2]) + ".png").c_str(), argv[2]);
    return 0;
 }
