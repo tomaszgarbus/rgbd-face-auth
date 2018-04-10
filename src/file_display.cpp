64563211e4b18b2d544b7a3201adead0f2b11ca2
Łukasz Raszkiewicz <lraszkiewicz@gmail.com>
2018-04-11 00:53:09 +0200

    Add IR support to file_display

--- src/file_display.cpp
+++ src/file_display.cpp
@@ -36,7 +36,7 @@ class DisplayPanel : public wxPanel {
 
 class SettingsPanel : public wxPanel {
  public:
-   explicit SettingsPanel(wxPanel *parent, MainWindow *window);
+   explicit SettingsPanel(wxPanel *parent, MainWindow *window, int min_slider_default, int max_slider_default, int slider_max);
 
    void on_min_slider_change(wxCommandEvent &event);
    void on_max_slider_change(wxCommandEvent &event);
@@ -61,12 +61,12 @@ class MainWindow : public wxFrame {
 
 // Definitions
 
-SettingsPanel::SettingsPanel(wxPanel *parent, MainWindow *window)
+SettingsPanel::SettingsPanel(wxPanel *parent, MainWindow *window, int min_slider_default, int max_slider_default, int slider_max)
       : wxPanel(parent, -1, wxPoint(-1, -1), wxSize(-1, -1), wxBORDER_SUNKEN), m_parent(parent), window(window),
-        m_min_d(new wxSlider(this, ID_MIN_D, 500, 0, 10000, wxPoint(80, 10), wxSize(500, 15))),
-        m_max_d(new wxSlider(this, ID_MAX_D, 1500, 0, 10000, wxPoint(80, 40), wxSize(500, 15))),
-        m_min_d_text(new wxTextCtrl(this, ID_MIN_D_TEXT, "500", wxPoint(10, 10), wxSize(60, 15))),
-        m_max_d_text(new wxTextCtrl(this, ID_MAX_D_TEXT, "4500", wxPoint(10, 40), wxSize(60, 15))) {
+        m_min_d(new wxSlider(this, ID_MIN_D, min_slider_default, 0, slider_max, wxPoint(80, 10), wxSize(500, 15))),
+        m_max_d(new wxSlider(this, ID_MAX_D, max_slider_default, 0, slider_max, wxPoint(80, 40), wxSize(500, 15))),
+        m_min_d_text(new wxTextCtrl(this, ID_MIN_D_TEXT, std::to_string(min_slider_default), wxPoint(10, 10), wxSize(60, 15))),
+        m_max_d_text(new wxTextCtrl(this, ID_MAX_D_TEXT, std::to_string(max_slider_default), wxPoint(10, 40), wxSize(60, 15))) {
    m_min_d->Bind(wxEVT_SCROLL_CHANGED, &SettingsPanel::on_min_slider_change, this);
    m_min_d->Bind(wxEVT_SCROLL_THUMBTRACK, &SettingsPanel::on_min_slider_change, this);
    m_max_d->Bind(wxEVT_SCROLL_CHANGED, &SettingsPanel::on_max_slider_change, this);
@@ -152,16 +152,23 @@ void DisplayPanel::refresh_display(wxCommandEvent &event) {
       val           = std::min(val, 255.0f);
       val           = std::max(val, 0.0f);
       int_pixels[i] = static_cast<uint8_t>(val);
+      if (!frame->is_depth) {
+         bitmap[3 * i] = int_pixels[i];
+         bitmap[3 * i + 1] = int_pixels[i];
+         bitmap[3 * i + 2] = int_pixels[i];
+      }
    }
-   cv::Mat current_image(cv::Size(static_cast<int>(width), static_cast<int>(height)), CV_8UC1, int_pixels);
-   cv::Mat destination_image(cv::Size(static_cast<int>(width), static_cast<int>(height)), CV_8UC3);
-   cv::applyColorMap(current_image, destination_image, cv::COLORMAP_RAINBOW);
-   for (size_t i = 0; i < height; ++i) {
-      for (size_t j = 0; j < width; ++j) {
-         auto pixel                      = destination_image.at<cv::Vec3b>(static_cast<int>(i), static_cast<int>(j));
-         bitmap[3 * (i * width + j)]     = pixel[2];
-         bitmap[3 * (i * width + j) + 1] = pixel[1];
-         bitmap[3 * (i * width + j) + 2] = pixel[0];
+   if (frame->is_depth) {
+      cv::Mat current_image(cv::Size(static_cast<int>(width), static_cast<int>(height)), CV_8UC1, int_pixels);
+      cv::Mat destination_image(cv::Size(static_cast<int>(width), static_cast<int>(height)), CV_8UC3);
+      cv::applyColorMap(current_image, destination_image, cv::COLORMAP_RAINBOW);
+      for (size_t i = 0; i < height; ++i) {
+         for (size_t j = 0; j < width; ++j) {
+            auto pixel = destination_image.at<cv::Vec3b>(static_cast<int>(i), static_cast<int>(j));
+            bitmap[3 * (i * width + j)] = pixel[2];
+            bitmap[3 * (i * width + j) + 1] = pixel[1];
+            bitmap[3 * (i * width + j) + 2] = pixel[0];
+         }
       }
    }
    m_picture = new wxStaticBitmap(this, wxID_ANY,
@@ -172,8 +179,22 @@ void DisplayPanel::refresh_display(wxCommandEvent &event) {
 MainWindow::MainWindow(const wxString &title, Picture::DepthOrIrFrame *frame)
       : wxFrame(nullptr, wxID_ANY, title, wxDefaultPosition,
               wxSize(static_cast<int>(frame->pixels->width + 150), static_cast<int>(frame->pixels->height + 200))),
-        m_parent(new wxPanel(this, wxID_ANY)), m_display(new DisplayPanel(m_parent, ID_DISPLAY, this, frame)),
-        m_settings(new SettingsPanel(m_parent, this)) {
+        m_parent(new wxPanel(this, wxID_ANY)), m_display(new DisplayPanel(m_parent, ID_DISPLAY, this, frame)) {
+   int min_slider_default, max_slider_default, slider_max;
+   float max_value = 0.0;
+   for (size_t i = 0; i < frame->pixels->width * frame->pixels->height; ++i) {
+      max_value = std::max(max_value, frame->pixels->data()[i]);
+   }
+   if (frame->is_depth) {
+      min_slider_default = 500;
+      slider_max = static_cast<int>(max_value);
+      max_slider_default = std::min(1500, slider_max);
+   } else {
+      min_slider_default = 0;
+      max_slider_default = static_cast<int>(max_value);
+      slider_max = max_slider_default;
+   }
+   m_settings = new SettingsPanel(m_parent, this, min_slider_default, max_slider_default, slider_max);
    auto vbox = new wxBoxSizer(wxVERTICAL);
    vbox->Add(m_display, 1, wxEXPAND | wxALL, 5);
    vbox->Add(m_settings, 1, wxEXPAND | wxALL, 5);
