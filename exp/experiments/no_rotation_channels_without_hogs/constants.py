e1b47c517abb758f5f85fc5047a01ea7e9dc131e
Tomasz Garbus <tomasz.garbus1@gmail.com>
2018-05-30 04:04:43 +0200

    Experiment: 4 channels, no hogs

---
+++ exp/experiments/no_rotation_channels_without_hogs/constants.py
@@ -0,0 +1,8 @@
+from common.constants import IMG_SIZE
+
+EXP_NAME = 'no_rotation_channels_with_hogs'
+
+"""
+    Size of the input images to the neural network.
+"""
+NN_INPUT_SIZE = [IMG_SIZE, IMG_SIZE, 4]
