3361f3f0a554a1aa5662eb52155c1aa62f2ed588
Dominik Klemba <D.Klemba@students.mimuw.edu.pl>
2018-05-30 13:47:18 +0200

    Cleaning experiments
    As main point of that branch is to merge tools into master.

--- face_auth/experiments/no_rotation_channels_with_hogs/constants.py
+++
@@ -1,8 +0,0 @@
-from common.constants import IMG_SIZE
-
-EXP_NAME = 'no_rotation_channels_with_hogs'
-
-"""
-    Size of the input images to the neural network.
-"""
-NN_INPUT_SIZE = [IMG_SIZE, IMG_SIZE, 6]
