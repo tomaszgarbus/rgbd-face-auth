7fb3c30267694290a90b9b47743bc3ee72dfd17d
Łukasz Raszkiewicz <lraszkiewicz@gmail.com>
2018-06-02 00:55:11 +0200

    Add copyright notice to C++ source files

--- src/libkinect.hpp
+++ src/libkinect.hpp
@@ -1,3 +1,22 @@
+/*
+   Novelty face authentication with liveness detection using depth and IR camera
+   Copyright (C) 2017-2018
+   Tomasz Garbus, Dominik Klemba, Jan Ludziejewski, Łukasz Raszkiewicz
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifndef LIBKINECT_HPP
 #define LIBKINECT_HPP
 
